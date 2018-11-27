2c64894603fb3e0d1344976b5e018fdb42c5c289
Dave Sproson <davesproson@gmail.com>
2018-11-27 03:36:01 +0000

    Allow the specification of WORDPRESS_COOKIEHASH in the django settings

--- wordpress_auth/__init__.py
+++ wordpress_auth/__init__.py
@@ -5,3 +5,4 @@ from django.conf import settings
 WORDPRESS_TABLE_PREFIX = getattr(settings, 'WORDPRESS_TABLE_PREFIX', 'wp_')
 WORDPRESS_LOGGED_IN_KEY = getattr(settings, 'WORDPRESS_LOGGED_IN_KEY')
 WORDPRESS_LOGGED_IN_SALT = getattr(settings, 'WORDPRESS_LOGGED_IN_SALT')
+WORDPRESS_COOKIEHASH = getattr(settings, 'WORDPRESS_COOKIEHASH', None)
